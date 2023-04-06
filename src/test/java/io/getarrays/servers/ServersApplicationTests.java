6aded163d3785efbbb3b97173b1947d3b3b6eb8b
navjeetrathore18 <email2navjeet@gmail.com>
2023-04-06 13:54:18 +0530

    SpringBoot First Commit

---
+++ src/test/java/io/getarrays/servers/ServersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getarrays.servers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
